674ecc7308447a6430bc615db0aea9616fe61e3d
Swapnil5101 <swapnil.nagar4774@gmail.com>
2025-07-01 00:15:27 +0530

    folder structure and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Medibot GenAI',
+    version = '0.0.0',
+    author = 'Swapnil Nagar',
+    author_email = 'swapnil.nagar4774@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
